6d92e92c7b829ea0ce60693c5a73187f4cab3d8f
Koy Saeteurn <kosatun@aol.com>
2018-08-16 22:44:04 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT bears.temperament, COUNT(bears.temperament) GROUP BY bears.temperament ORDER BY COUNT(*) DESC LIMIT 1;"
+  "SELECT bears.temperament, COUNT(bears.temperament) WHERE bears.temperament == 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
